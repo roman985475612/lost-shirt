10f8c42942f591ad5d1a539bc4288eaa498d2cf5
Roman <roman985475612@gmail.com>
2021-06-25 10:54:33 +0300

    Part completed

--- gulpfile.js
+++ gulpfile.js
@@ -1,49 +1,112 @@
-const { src, dest, task, series, watch } = require('gulp')
+const { src, dest, task, series, watch, parallel } = require('gulp')
+const { DIST_PATH, SRC_PATH, STYLES_LIBS, JS_LIBS } = require('./gulp.config')
 const rm = require('gulp-rm')
 const sass = require('gulp-sass')
 const concat = require('gulp-concat')
 const browserSync = require('browser-sync').create()
 const reload = browserSync.reload
 const sassGlob = require('gulp-sass-glob')
+const autoprefixer = require('gulp-autoprefixer')
+const px2rem = require('gulp-smile-px2rem')
+const gcmq = require('gulp-group-css-media-queries')
+const cleanCss = require('gulp-clean-css')
+const sourcemaps = require('gulp-sourcemaps')
+const babel = require('gulp-babel')
+const uglify = require('gulp-uglify')
+const svgo = require('gulp-svgo')
+const svgSprite = require('gulp-svg-sprite')
+const gulpIf = require('gulp-if')
 
 sass.compiler = require('node-sass')
 
+const env = process.env.NODE_ENV
 
 task('clean', () => {
-    return src('dist/**/*', {read: false})
+    return src(`${DIST_PATH}/**/*`, {read: false})
         .pipe(rm())
 })
 
 task('copy:html', () => {
-    return src('src/*.html')
-        .pipe(dest('dist'))
+    return src(`${SRC_PATH}/*.html`)
+        .pipe(dest(`${DIST_PATH}`))
         .pipe(reload({stream: true}))
 })
 
-const styles = [
-    'node_modules/normalize.css/normalize.css',
-    'src/styles/main.scss'
-]
-
 task('styles', () => {
-    return src(styles)
-        .pipe(concat('main.css'))
+    return src([...STYLES_LIBS, `${SRC_PATH}/styles/main.scss`])
+        .pipe(gulpIf(env === 'dev', sourcemaps.init()))
+        .pipe(concat('main.min.scss'))
         .pipe(sassGlob())
         .pipe(sass().on('error', sass.logError))
-        .pipe(dest('dist/css'))
+        .pipe(px2rem())
+        .pipe(gulpIf(env === 'prod', autoprefixer({
+            browsers: ['last 2 versions'],
+            cascade: true
+        })))
+        .pipe(gulpIf(env === 'prod', gcmq()))
+        .pipe(gulpIf(env === 'prod', cleanCss()))
+        .pipe(gulpIf(env === 'dev', sourcemaps.write()))
+        .pipe(dest(`${DIST_PATH}/css`))
         .pipe(reload({stream: true}))
+})
+
+task('scripts', () => {
+    return src([...JS_LIBS, `${SRC_PATH}/scripts/*.js`])
+        .pipe(gulpIf(env === 'dev', sourcemaps.init()))
+        .pipe(concat('main.min.js', {newLine: ';'}))
+        .pipe(gulpIf(env === 'prod', babel({
+            presets: ["@babel/env"]
+        })))
+        .pipe(gulpIf(env === 'prod', uglify()))
+        .pipe(gulpIf(env === 'dev', sourcemaps.write()))
+        .pipe(dest(`${DIST_PATH}/js`))
+        .pipe(reload({stream: true}))
+})
+
+task('icons', () => {
+    return src(`${SRC_PATH}/images/icons/*.svg`)
+        .pipe(svgo({
+            plugins: [
+                {
+                    removeAttrs: {
+                        attrs: "(fill|stroke|style|width|data.*)"
+                    }
+                }
+            ]
+        }))
+        .pipe(svgSprite({
+            mode: {
+                symbol: {
+                    sprite: "../sprite.svg"
+                }
+            }
+        }))
+        .pipe(dest(`${DIST_PATH}/images/icons`))
 })
 
 task('server', () => {
     browserSync.init({
         server: {
-            baseDir: './dist'
+            baseDir: `./${DIST_PATH}`
         },
         open: false
     })
 })
 
-watch('./src/styles/**/*.scss', series('styles'))
-watch('./src/*.html', series('copy:html'))
+task('watch', () => {
+    watch(`./${SRC_PATH}/styles/**/*.scss`, series('styles'))
+    watch(`./${SRC_PATH}/scripts/**/*.js`, series('scripts'))
+    watch(`./${SRC_PATH}/*.html`, series('copy:html'))
+    watch(`./${SRC_PATH}/images/icons/*.svg`, series('icons'))    
+})
 
-task('default', series('clean', 'copy:html', 'styles', 'server'))
+task(
+    'default', 
+    series('clean', parallel('copy:html', 'styles', 'scripts', 'icons')), 
+    parallel('watch', 'server')
+)
+
+task(
+    'build', 
+    series('clean', parallel('copy:html', 'styles', 'scripts', 'icons')) 
+)
